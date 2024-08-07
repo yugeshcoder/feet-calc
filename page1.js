b01776aa7a530eca50b94600a9059c59ca47c621
yugeshcoder <143157324+yugeshcoder@users.noreply.github.com>
2024-08-07 20:38:38 +0530

    Add files via upload

---
+++ page1.js
@@ -0,0 +1,346 @@
+
+
+let f1 = 8;
+let p1 = 0;
+let t1 = 0;
+let i1 = 0;
+
+document.getElementById("s1").onclick = function(){
+  i1 = Number(document.getElementById("n1").value);
+  p1 += i1;
+  document.getElementById("p1").textContent = p1;
+  t1 = f1*p1;
+  document.getElementById("t1").textContent = t1;
+}
+
+let f2 = 7.79;
+let p2 = 0;
+let t2 = 0;
+let i2 = 0;
+
+document.getElementById("s2").onclick = function(){
+  i2 = Number(document.getElementById("n2").value);
+  p2 += i2;
+  document.getElementById("p2").textContent = p2;
+  t2 = f2*p2;
+  document.getElementById("t2").textContent = t2;
+}
+
+let f3 = 7.50;
+let p3 = 0;
+let t3 = 0;
+let i3 = 0;
+
+document.getElementById("s3").onclick = function(){
+  i3 = Number(document.getElementById("n3").value);
+  p3 += i3;
+  document.getElementById("p3").textContent = p3;
+  t3 = f3*p3;
+  document.getElementById("t3").textContent = t3;
+}
+
+let f4 = 7.25;
+let p4 = 0;
+let t4 = 0;
+let i4 = 0;
+
+document.getElementById("s4").onclick = function(){
+  i4 = Number(document.getElementById("n4").value);
+  p4 += i4;
+  document.getElementById("p4").textContent = p4;
+  t4 = f4*p4;
+  document.getElementById("t4").textContent = t4;
+}
+
+let f5 = 7.0;
+let p5 = 0;
+let t5 = 0;
+let i5 = 0;
+
+document.getElementById("s5").onclick = function(){
+  i5 = Number(document.getElementById("n5").value);
+  p5 += i5;
+  document.getElementById("p5").textContent = p5;
+  t5 = f5*p5;
+  document.getElementById("t5").textContent = t5;
+}
+
+let f6 = 6.75;
+let p6 = 0;
+let t6 = 0;
+let i6 = 0;
+
+document.getElementById("s6").onclick = function(){
+  i6 = Number(document.getElementById("n6").value);
+  p6 += i6;
+  document.getElementById("p6").textContent = p6;
+  t6 = f6*p6;
+  document.getElementById("t6").textContent = t6;
+}
+
+let f7 = 6.50;
+let p7 = 0;
+let t7 = 0;
+let i7 = 0;
+
+document.getElementById("s7").onclick = function(){
+  i7 = Number(document.getElementById("n7").value);
+  p7 += i7;
+  document.getElementById("p7").textContent = p7;
+  t7 = f7*p7;
+  document.getElementById("t7").textContent = t7;
+}
+
+let f8 = 6.25;
+let p8 = 0;
+let t8 = 0;
+let i8 = 0;
+
+document.getElementById("s8").onclick = function(){
+  i8 = Number(document.getElementById("n8").value);
+  p8 += i8;
+  document.getElementById("p8").textContent = p8;
+  t8= f8*p8;
+  document.getElementById("t8").textContent = t8;
+}
+
+let f9 = 6.0;
+let p9 = 0;
+let t9 = 0;
+let i9 = 0;
+
+document.getElementById("s9").onclick = function(){
+  i9 = Number(document.getElementById("n9").value);
+  p9 += i9;
+  document.getElementById("p9").textContent = p9;
+  t9 = f9*p9;
+  document.getElementById("t9").textContent = t9;
+}
+
+let f10 = 5.75;
+let p10 = 0;
+let t10 = 0;
+let i10 = 0;
+
+document.getElementById("s10").onclick = function(){
+  i10 = Number(document.getElementById("n10").value);
+  p10 += i10;
+  document.getElementById("p10").textContent = p10;
+  t10 = f10*p10;
+  document.getElementById("t10").textContent = t10;
+}
+
+let f11 = 5.5;
+let p11 = 0;
+let t11 = 0;
+let i11 = 0;
+
+document.getElementById("s11").onclick = function(){
+  i11 = Number(document.getElementById("n11").value);
+  p11 += i11;
+  document.getElementById("p11").textContent = p11;
+  t11 = f11*p11;
+  document.getElementById("t11").textContent = t11;
+}
+
+let f12 = 5.25;
+let p12 = 0;
+let t12 = 0;
+let i12 = 0;
+
+document.getElementById("s12").onclick = function(){
+  i12 = Number(document.getElementById("n12").value);
+  p12 += i12;
+  document.getElementById("p12").textContent = p12;
+  t12 = f12*p12;
+  document.getElementById("t12").textContent = t12;
+}
+
+let f13 = 5.0;
+let p13 = 0;
+let t13 = 0;
+let i13 = 0;
+
+document.getElementById("s13").onclick = function(){
+  i13 = Number(document.getElementById("n13").value);
+  p13 += i13;
+  document.getElementById("p13").textContent = p13;
+  t13 = f13*p13;
+  document.getElementById("t13").textContent = t13;
+}
+
+let f14 = 4.75;
+let p14 = 0;
+let t14 = 0;
+let i14 = 0;
+
+document.getElementById("s14").onclick = function(){
+  i14 = Number(document.getElementById("n14").value);
+  p14 += i14;
+  document.getElementById("p14").textContent = p14;
+  t14 = f14*p14;
+  document.getElementById("t14").textContent = t14;
+}
+
+let f15 = 4.50;
+let p15 = 0;
+let t15 = 0;
+let i15 = 0;
+
+document.getElementById("s15").onclick = function(){
+  i15 = Number(document.getElementById("n15").value);
+  p15 += i15;
+  document.getElementById("p15").textContent = p15;
+  t15 = f15*p15;
+  document.getElementById("t15").textContent = t15;
+}
+
+let f16 = 4.25;
+let p16 = 0;
+let t16 = 0;
+let i16 = 0;
+
+document.getElementById("s16").onclick = function(){
+  i16 = Number(document.getElementById("n16").value);
+  p16 += i16;
+  document.getElementById("p16").textContent = p16;
+  t16 = f16*p16;
+  document.getElementById("t16").textContent = t16;
+}
+
+let f17 = 4.0;
+let p17 = 0;
+let t17 = 0;
+let i17 = 0;
+
+document.getElementById("s17").onclick = function(){
+  i17 = Number(document.getElementById("n17").value);
+  p17 += i17;
+  document.getElementById("p17").textContent = p17;
+  t17 = f17*p17;
+  document.getElementById("t17").textContent = t17;
+}
+
+let f18 = 3.75;
+let p18 = 0;
+let t18 = 0;
+let i18 = 0;
+
+document.getElementById("s18").onclick = function(){
+  i18 = Number(document.getElementById("n18").value);
+  p18 += i18;
+  document.getElementById("p18").textContent = p18;
+  t18 = f18*p18;
+  document.getElementById("t18").textContent = t18;
+}
+
+let f19 = 3.5;
+let p19 = 0;
+let t19 = 0;
+let i19 = 0;
+
+document.getElementById("s19").onclick = function(){
+  i19 = Number(document.getElementById("n19").value);
+  p19 += i19;
+  document.getElementById("p19").textContent = p19;
+  t19 = f19*p19;
+  document.getElementById("t19").textContent = t19;
+}
+
+let f20 = 3.25;
+let p20 = 0;
+let t20 = 0;
+let i20 = 0;
+
+document.getElementById("s20").onclick = function(){
+  i20 = Number(document.getElementById("n20").value);
+  p20 += i20;
+  document.getElementById("p20").textContent = p20;
+  t20 = f20*p20;
+  document.getElementById("t20").textContent = t20;
+}
+
+let f21 = 3.0;
+let p21 = 0;
+let t21 = 0;
+let i21 = 0;
+
+document.getElementById("s21").onclick = function(){
+  i21 = Number(document.getElementById("n21").value);
+  p21 += i21;
+  document.getElementById("p21").textContent = p21;
+  t21 = f21*p21;
+  document.getElementById("t21").textContent = t21;
+}
+
+let f22 = 2.75;
+let p22 = 0;
+let t22 = 0;
+let i22 = 0;
+
+document.getElementById("s22").onclick = function(){
+  i22 = Number(document.getElementById("n22").value);
+  p22 += i22;
+  document.getElementById("p22").textContent = p22;
+  t22 = f22*p22;
+  document.getElementById("t22").textContent = t22;
+}
+
+let f23 = 2.5;
+let p23 = 0;
+let t23 = 0;
+let i23 = 0;
+
+document.getElementById("s23").onclick = function(){
+  i23 = Number(document.getElementById("n23").value);
+  p23 += i23;
+  document.getElementById("p23").textContent = p23;
+  t23 = f23*p23;
+  document.getElementById("t23").textContent = t23;
+}
+
+let f24 = 2.25;
+let p24 = 0;
+let t24 = 0;
+let i24 = 0;
+
+document.getElementById("s24").onclick = function(){
+  i24 = Number(document.getElementById("n24").value);
+  p24 += i24;
+  document.getElementById("p24").textContent = p24;
+  t24 = f24*p24;
+  document.getElementById("t24").textContent = t24;
+}
+
+let f25 = 2.0;
+let p25 = 0;
+let t25 = 0;
+let i25 = 0;
+
+document.getElementById("s25").onclick = function(){
+  i25 = Number(document.getElementById("n25").value);
+  p25 += i25;
+  document.getElementById("p25").textContent = p25;
+  t25 = f25*p25;
+  document.getElementById("t25").textContent = t25;
+}
+
+let total = 0;
+document.getElementById("submit").onclick = function(){
+  total = t1+t2+t3+t4+t5+t6+t7+t8+t9+t10+t11+t12+t13+t14+t15+t16+t17+t18+t19+t20+t21+t22+t23+t24+t25;
+  document.getElementById("total").textContent = total;
+}
+
+
+const btn = document.querySelector('.screenshot');
+
+btn.addEventListener('click', () => {
+  html2canvas(document.querySelector('#mob'))
+    .then(canvas => {
+      const url = canvas.toDataURL('image/png');
+      const a = document.createElement('a');
+      a.setAttribute('download','imageName.png');
+      a.setAttribute('href',url);
+      a.click();
+    })
+})
